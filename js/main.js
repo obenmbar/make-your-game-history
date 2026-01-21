9ecac6b237a96028551e435552fdba3e9200fbd3
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2026-01-21 08:53:25 +0100

    Add story modal and improve game state handling

--- js/main.js
+++ js/main.js
@@ -3,17 +3,30 @@ import { state } from './gameState.js';
 import * as UI from './ui.js';
 import * as Physics from './physics.js';
 import * as Entities from './entities.js';
-import { PADDLE_SPEED } from './constants.js';
+import { PADDLE_SPEED, STORY } from './constants.js';
 
 export const gameArea = document.getElementById('game-area');
 export const paddle = document.getElementById('paddle');
 export const ball = document.getElementById('ball');
 export const brickContainer = document.getElementById('bricks-container');
 export const conpausediv = document.getElementById('pause_continue_messege')
+
 Entities.initBricks(gameArea.clientWidth, brickContainer);
 Entities.resetPositions(ball, paddle, gameArea.clientWidth, gameArea.clientHeight);
 UI.displayLives();
 UI.updatelevel()
+
+window.onload = () => {
+    UI.showModal(
+        STORY.INTRO.title,
+        STORY.INTRO.text,
+        STORY.INTRO.btn,
+        () => {
+         UI.modal.style.display = 'none'
+          state.showstory = true
+        }
+    );
+};
 
 
 export function handleResetSequence(isNewLevel = false) {
@@ -54,25 +67,25 @@ export function handleResetSequence(isNewLevel = false) {
 document.addEventListener('keydown', (e) => {
 
     if (state.isResetting) return;
+    if(!state.showstory)return;
 
     if (e.key === 'ArrowRight' || e.code === 'KeyF') state.rightPressed = true;
     if (e.key === 'ArrowLeft' || e.code === 'KeyS') state.leftPressed = true;
 
     if (e.code === 'Space') {
 
-        if (!state.gameRunning && !state.isResetting) {
+        if (!state.gameRunning && !state.isResetting && state.showstory) {
             handleResetSequence(true);
             conpausediv.style.display = 'block'
             conpausediv.innerText = 'Press Space to Pause'
         } else {
             state.isPaused = !state.isPaused
 
-         
-            if (state.isPaused) {
+            if (state.isPaused && state.showstory) {
                 conpausediv.innerText = 'Press Space to Continue'
-            } else {
+            } else if(state.showstory) {
                 conpausediv.innerText = 'Press Space to Pause'
-             
+
             }
         }
 
@@ -94,71 +107,82 @@ window.addEventListener('resize', () => {
 
 function gameLoop() {
     if (!state.isPaused) {
-        
 
-    if (!state.isResetting) {
-        if (state.rightPressed && state.currentPaddleX < gameArea.clientWidth - paddle.offsetWidth) {
-            state.currentPaddleX += PADDLE_SPEED;
-        } else if (state.leftPressed && state.currentPaddleX > 0) {
-            state.currentPaddleX -= PADDLE_SPEED;
+
+        if (!state.isResetting) {
+            if (state.rightPressed && state.currentPaddleX < gameArea.clientWidth - paddle.offsetWidth) {
+                state.currentPaddleX += PADDLE_SPEED;
+            } else if (state.leftPressed && state.currentPaddleX > 0) {
+                state.currentPaddleX -= PADDLE_SPEED;
+            }
+            paddle.style.transform = `translateX(${state.currentPaddleX}px)`;
         }
-        paddle.style.transform = `translateX(${state.currentPaddleX}px)`;
-    }
 
-    if (!state.gameRunning) {
-        state.ballX = state.currentPaddleX + (paddle.offsetWidth / 2) - (ball.offsetWidth / 2);
-        ball.style.transform = `translate(${state.ballX}px, ${state.ballY}px)`;
-    }
+        if (!state.gameRunning) {
+            state.ballX = state.currentPaddleX + (paddle.offsetWidth / 2) - (ball.offsetWidth / 2);
+            ball.style.transform = `translate(${state.ballX}px, ${state.ballY}px)`;
+        }
 
 
-    if (state.gameRunning) {
-        state.ballX += state.ballSpeedX;
-        state.ballY += state.ballSpeedY;
+        if (state.gameRunning) {
+            state.ballX += state.ballSpeedX;
+            state.ballY += state.ballSpeedY;
 
 
-        if (state.ballX + ball.offsetWidth >= gameArea.clientWidth || state.ballX <= 0) {
-            if (state.ballX <= 0) {
-                state.ballX = 0;
-            } else {
-                state.ballX = gameArea.clientWidth - ball.offsetWidth
+            if (state.ballX + ball.offsetWidth >= gameArea.clientWidth || state.ballX <= 0) {
+                if (state.ballX <= 0) {
+                    state.ballX = 0;
+                } else {
+                    state.ballX = gameArea.clientWidth - ball.offsetWidth
+                }
+
+                state.ballSpeedX *= -1;
+            }
+            if (state.ballY <= 0) {
+                state.ballSpeedY *= -1;
             }
 
-            state.ballSpeedX *= -1;
-        }
-        if (state.ballY <= 0) {
-            state.ballSpeedY *= -1;
-        }
+            Physics.handleBallPaddleCollision(paddle.offsetTop, state.currentPaddleX, paddle.offsetWidth, ball.offsetWidth);
+            Physics.detectCollision(ball.offsetWidth);
 
-        Physics.handleBallPaddleCollision(paddle.offsetTop, state.currentPaddleX, paddle.offsetWidth, ball.offsetWidth);
-        Physics.detectCollision(ball.offsetWidth);
+            if (state.ballY + ball.offsetWidth >= gameArea.clientHeight) {
+                state.lives--;
+                UI.displayLives();
 
-        if (state.ballY + ball.offsetWidth >= gameArea.clientHeight) {
-            state.lives--;
-            UI.displayLives();
+                if (state.lives === 0) {
+                    state.gameRunning = false,
+                    state.showstory = false
+                        UI.showModal(
+                              STORY.LOSE.title,
+                              STORY.LOSE.text,
+                              STORY.LOSE.btn,
+                        ()=>{
+                            document.location.reload()
+                            state.showstory = true
+                        }
+                        );
+                } else {
 
-            if (state.lives === 0) {
-                state.gameRunning = false,
-                    UI.showModal('GAME OVER',
-                        "Hard luck! Frieza was too strong.",
-                        "TRY AGAIN",
-                        () => document.location.reload())
-            } else {
+                    handleResetSequence();
+                }
+            } else if (state.timerSecond === 0) {
+                state.gameRunning = false;
+                state.showstory= false
 
-                handleResetSequence();
+                UI.showModal(
+                   STORY.TIMEOUT.title,
+                   STORY.TIMEOUT.text,
+                   STORY.TIMEOUT.btn,
+                    () => {
+                        document.location.reload()
+                        state.showstory = true
+                    }
+                );
             }
-        } else if (state.timerSecond === 0) {
-            state.gameRunning = false;
-            UI.showModal(
-                "TIME OUT!",
-                "You ran out of time! Frieza escaped.",
-                "TRY AGAIN",
-                () => document.location.reload()
-            );
-        }
 
-        ball.style.transform = `translate(${state.ballX}px, ${state.ballY}px)`;
+            ball.style.transform = `translate(${state.ballX}px, ${state.ballY}px)`;
+        }
     }
-}
     requestAnimationFrame(gameLoop);
 }
 
